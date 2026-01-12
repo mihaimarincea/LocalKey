fbefe34eb74c066211ac861353282d2a775c2ac2
Mihai Marincea <mihai.marincea@gmail.com>
2026-01-12 21:06:50 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-368226286-b97b0",
+  "appId": "1:268264251022:web:9c376b2c66867bdd13b3df",
+  "apiKey": "AIzaSyBkMsDsuGUdNxoZOj1QScQiv1GeZMe2Las",
+  "authDomain": "studio-368226286-b97b0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "268264251022"
+};
