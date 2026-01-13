14b425e2d1de1fbc090a18152125f6523d42537a
Mihai Marincea <mihai.marincea@gmail.com>
2026-01-13 08:00:23 +0000

    login page-ul are un height ciudat, poate de la imagine face scroll dege

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -52,7 +52,7 @@ export default function AuthPage() {
   }
 
   return (
-    <div className="w-full h-screen lg:grid lg:grid-cols-2">
+    <div className="w-full min-h-screen lg:grid lg:grid-cols-2">
       <div className="flex items-center justify-center py-12">
         <div className="mx-auto grid w-[350px] gap-6">
           <div className="grid gap-2 text-center">
