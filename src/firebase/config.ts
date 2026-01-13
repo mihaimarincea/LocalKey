cca9fa1322168efa5c4bf819ffab4d3944a2cc9d
Mihai Marincea <mihai.marincea@gmail.com>
2026-01-13 14:15:38 +0000

    gs://studio-368226286-b97b0.firebasestorage.app aici este bucket-ul

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:268264251022:web:9c376b2c66867bdd13b3df",
   "apiKey": "AIzaSyBkMsDsuGUdNxoZOj1QScQiv1GeZMe2Las",
   "authDomain": "studio-368226286-b97b0.firebaseapp.com",
+  "storageBucket": "studio-368226286-b97b0.appspot.com",
   "measurementId": "",
   "messagingSenderId": "268264251022"
 };
